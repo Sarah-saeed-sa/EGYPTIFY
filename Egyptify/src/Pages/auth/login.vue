74e3724e6b7d70df63964ca3f09f7d726e5eef1f
Sondos Reda <sondos.elghazawy@gmail.com>
2025-07-25 09:01:37 +0300

    Egyptify/src/components/InputField.vue

--- Egyptify/src/Pages/auth/login.vue
+++ Egyptify/src/Pages/auth/login.vue
@@ -1,9 +1,9 @@
-<template>
+<!-- <template>
   <div class="p-8 max-w-md mx-auto">
     <h2 class="text-xl font-bold mb-4">Login</h2>
     <input v-model="email" placeholder="Email" class="border p-2 w-full mb-2" />
     <input v-model="password" type="password" placeholder="Password" class="border p-2 w-full mb-2" />
-    <!-- <button @click="handleLogin" class="text-white px-4 py-2 rounded">Login</button> -->
+    <button @click="handleLogin" class="text-white px-4 py-2 rounded">Login</button>
      <BaseButton>Click Me</BaseButton>
   </div>
 </template>
@@ -30,4 +30,11 @@ function handleLogin() {
     alert('Wrong email or password!')
   }
 }
+</script> -->
+<script setup>
+import LoginForm from '../../components/LoginForm.vue'
 </script>
+
+<template>
+  <LoginForm />
+</template>
